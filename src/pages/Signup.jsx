24d73c280b3b92927cddd325860d80ebf8fd2fa3
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-06-15 09:38:08 +0500

    complete: signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Google from "../assets/Signup/Google.svg"
 import Facebook from "../assets/Signup/Facebook.svg"
+import Image1 from '../assets/Signup/Image1.png'
+import Image2 from '../assets/Signup/Image2.png'
 
 export default function Signup() {
   return (
-    <main className='h-screen flex'>
-      <div className='bg-primary2 w-[49.306vw] h-full'></div>
+    <main className='flex'>
+      <div className='blurSignup bg-primary2 w-[49.306vw] h-[846px] flex gap-[13px] justify-center overflow-hidden'>
+        <img className='w-[283px] relative top-[45px]' src={Image1} alt="" />
+        <img className='w-[283px]' src={Image2} alt="" />
+      </div>
 
-      <div className='bg-[white] w-[50.694vw] h-full flex flex-col items-center pt-[105px]'>
+      <div className='bg-[white] w-[50.694vw] h-full flex flex-col items-center pt-[105px] '>
         <div className='w-fit h-fit flex flex-col justify-center items-center'>
           <h1 className='font-rubik font-bold text-[32px] leading-[37.92px] tracking-[-0.02em]'>Sign up to <span className='text-primary2 underline'>COOK</span></h1>
           <div className='flex flex-col gap-[8px] mt-[24px]'>
@@ -28,7 +33,7 @@ export default function Signup() {
 
         <div>
           <h3 className='font-redHatDisplay font-bold text-[20px] leading-[26.46px] text-center'>Sign up with email</h3>
-          <form className='w-[346px] flex flex-col gap-[18px] items-center mt-[29px]'>
+          <form className='w-[346px] flex flex-col gap-[18px] items-center mt-[29px] mb-[62px]'>
             <div className='w-full flex gap-[18px]'>
               <div className='w-[164px] flex flex-col gap-[2px]'>
                 <label className='font-outfit font-medium text-[16px] leading-[20.16px]' htmlFor="">First Name</label>
@@ -50,7 +55,7 @@ export default function Signup() {
               <input className='border border-[rgba(255,219,184,1)] h-[37px]' type="text" />
             </div>
 
-            <button className='text-[white] bg-primary2 h-[39px] w-[122px] rounded-[4px] font-outfit font-medium text-[18px] leading-[22.68px]'>Join COOK</button>
+            <button className='text-[white] bg-primary2 h-[39px] w-[122px] rounded-[4px] font-outfit font-medium text-[18px] leading-[22.68px] mt-[11px]'>Join COOK</button>
           </form>
         </div>
       </div>
