80c83bef7147c33575d0db221044563549f43fab
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-06-23 22:13:35 +0500

    fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -68,7 +68,7 @@ export default function LandingPage() {
               <div>
                 <h2 className='font-rubik text-[18px] font-medium leading-[21.33px]'>David Mitchell</h2>
                 <div className='flex items-center gap-[7px]'>
-                  <h3 className='font-outfit text-[16px] font-semibold leading-[20.16px]'>4r.8</h3>
+                  <h3 className='font-outfit text-[16px] font-semibold leading-[20.16px]'>4.8</h3>
                   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 1.5L10.163 5.77865L15 6.46898L11.5 9.79758L12.326 14.5L8 12.2787L3.674 14.5L4.5 9.79758L1 6.46898L5.837 5.77865L8 1.5Z" fill="#D27722" stroke="#D27722" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
